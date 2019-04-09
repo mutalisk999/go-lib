2acd432d60e06c84c4e69ee24efbae2a608c59e6
mutalisk <0x08@0x08.net>
2019-04-09 17:37:56 +0800

    add PriorityTaskMgr

--- src/task/priority_task_mgr/priority_task_mgr.go
+++ src/task/priority_task_mgr/priority_task_mgr.go
@@ -1,1 +1,120 @@
 package priority_task_mgr
+
+import (
+	"github.com/mutalisk999/go-lib/src/queue/blocking_priority_queue"
+	"github.com/mutalisk999/go-lib/src/sched/goroutine_mgr"
+	"strconv"
+)
+
+type PriorityTask struct {
+	taskFunc     func(goroutine_mgr.Goroutine, interface{}) error
+	taskArgs     interface{}
+	taskPriority interface{}
+}
+
+type PriorityTaskMgr struct {
+	taskMgrName       string
+	goroutineCount    uint64
+	goroutineMgr      *goroutine_mgr.GoroutineManager
+	goroutineQuit     []chan error
+	taskQueue         *blocking_priority_queue.BlockingPriorityQueue
+	taskQueueBlocking bool
+}
+
+func (t *PriorityTaskMgr) Initialise(taskMgrName string, goroutineCount uint64, taskCmpFunc func(l interface{}, r interface{}) int,
+	taskQueueBlocking bool, taskQueueMaxSize uint64) {
+	t.taskMgrName = taskMgrName
+	t.goroutineCount = goroutineCount
+	t.goroutineMgr = new(goroutine_mgr.GoroutineManager)
+	t.goroutineMgr.Initialise(t.taskMgrName + ".GoroutineMgr")
+	t.goroutineQuit = make([]chan error, 0)
+	t.taskQueue = new(blocking_priority_queue.BlockingPriorityQueue)
+	t.taskQueue.Initialise(2, taskCmpFunc, taskQueueMaxSize)
+	t.taskQueueBlocking = taskQueueBlocking
+}
+
+func (t *PriorityTaskMgr) PushTask(task PriorityTask) error {
+	if t.taskQueueBlocking {
+		t.taskQueue.PushBlocking(task)
+		return nil
+	} else {
+		err := t.taskQueue.Push(task)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (t *PriorityTaskMgr) PopTask() (PriorityTask, error) {
+	if t.taskQueueBlocking {
+		elem := t.taskQueue.PopBlocking()
+		return elem.(PriorityTask), nil
+	} else {
+		elem, err := t.taskQueue.Pop()
+		if err != nil {
+			return PriorityTask{}, err
+		}
+		return elem.(PriorityTask), nil
+	}
+}
+
+func (t *PriorityTaskMgr) runCallBackBlocking(g goroutine_mgr.Goroutine) {
+	defer g.OnQuit()
+	for {
+		task, _ := t.PopTask()
+		task.taskFunc(g, task.taskArgs)
+	}
+}
+
+func (t *PriorityTaskMgr) runCallBack(g goroutine_mgr.Goroutine, chanIndex interface{}, detach interface{}) {
+	defer g.OnQuit()
+	for {
+		task, err := t.PopTask()
+		if err != nil {
+			break
+		}
+		task.taskFunc(g, task.taskArgs)
+	}
+	if !detach.(bool) {
+		t.goroutineQuit[chanIndex.(int)] <- nil
+	}
+}
+
+func (t *PriorityTaskMgr) Run(detach bool) {
+	if t.taskQueueBlocking {
+		for i := 0; i < int(t.goroutineCount); i++ {
+			t.goroutineMgr.GoroutineCreateP0(t.taskMgrName+".Goroutine"+strconv.Itoa(i), t.runCallBackBlocking)
+		}
+	} else {
+		for i := 0; i < int(t.goroutineCount); i++ {
+			if !detach {
+				ch := make(chan error)
+				t.goroutineQuit = append(t.goroutineQuit, ch)
+			}
+			t.goroutineMgr.GoroutineCreateP2(t.taskMgrName+".Goroutine"+strconv.Itoa(i), t.runCallBack, i, detach)
+		}
+	}
+}
+
+func (t *PriorityTaskMgr) Wait() {
+	for i := 0; i < len(t.goroutineQuit); i++ {
+		<-t.goroutineQuit[i]
+	}
+}
+
+func (t *PriorityTaskMgr) RunAndWait(detach bool) {
+	t.Run(detach)
+	t.Wait()
+}
+
+func (t *PriorityTaskMgr) Destroy() {
+	t.taskMgrName = ""
+	t.goroutineCount = 0
+	t.goroutineMgr.Destroy()
+	t.goroutineMgr = nil
+	t.goroutineQuit = make([]chan error, 0)
+	t.taskQueue.Destroy()
+	t.taskQueue = nil
+	t.taskQueueBlocking = false
+}
--- src/task/priority_task_mgr/priority_task_mgr_test.go
+++ src/task/priority_task_mgr/priority_task_mgr_test.go
@@ -1,1 +1,44 @@
 package priority_task_mgr
+
+import (
+	"fmt"
+	"github.com/mutalisk999/go-lib/src/sched/goroutine_mgr"
+	"testing"
+	"time"
+)
+
+func taskCallBack(g goroutine_mgr.Goroutine, i interface{}) error {
+	fmt.Println("goroutine:", g.GetName(), "| print i:", i)
+	time.Sleep(1 * time.Millisecond)
+	return nil
+}
+
+func taskCmpFunc(l interface{}, r interface{}) int {
+	taskLeft := l.(PriorityTask)
+	taskRight := r.(PriorityTask)
+	taskPriorityLeft := taskLeft.taskPriority.(int)
+	taskPriorityRight := taskRight.taskPriority.(int)
+	if taskPriorityLeft < taskPriorityRight {
+		return -1
+	} else if taskPriorityLeft == taskPriorityRight {
+		return 0
+	} else {
+		return 1
+	}
+}
+
+func TestAll(t *testing.T) {
+	taskMgr := new(PriorityTaskMgr)
+	taskMgr.Initialise("TestTask", 5, taskCmpFunc, true, 0)
+	taskMgr.Run(true)
+
+	for index := 0; index < 10; index++ {
+		task := new(PriorityTask)
+		task.taskFunc = taskCallBack
+		task.taskArgs = index
+		task.taskPriority = index
+		taskMgr.PushTask(*task)
+	}
+
+	time.Sleep(1 * time.Second)
+}

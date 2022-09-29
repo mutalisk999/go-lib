85464484083308f144c739cc3e66706d4a995668
mutalisk999 <tangjian999999999@gmail.com>
2022-09-29 23:00:48 +0800

    add WithCancel functions

--- src/sched/goroutine_mgr/goroutine_mgr.go
+++ src/sched/goroutine_mgr/goroutine_mgr.go
@@ -1,16 +1,19 @@
 package goroutine_mgr
 
 import (
+	"context"
 	"fmt"
 	"sync"
-	. "time"
+	"time"
 )
 
 type Goroutine struct {
 	goroutineId         uint64
 	goroutineName       string
-	goroutineCreateTime Time
+	goroutineCreateTime time.Time
 	fromManager         *GoroutineManager
+	goroutineCtx        *context.Context
+	goroutineCancelFunc *context.CancelFunc
 }
 
 func (g *Goroutine) OnQuit() {
@@ -23,6 +26,14 @@ func (g *Goroutine) GetId() uint64 {
 
 func (g *Goroutine) GetName() string {
 	return g.goroutineName
+}
+
+func (g *Goroutine) GetContext() *context.Context {
+	return g.goroutineCtx
+}
+
+func (g *Goroutine) GetCancelFunc() *context.CancelFunc {
+	return g.goroutineCancelFunc
 }
 
 type GoroutineManager struct {
@@ -46,7 +57,25 @@ func (g *GoroutineManager) constructGoroutine(goroutineName string) *Goroutine {
 	goroutine.goroutineId = g.counter
 	g.mutex.Unlock()
 	goroutine.goroutineName = goroutineName
-	goroutine.goroutineCreateTime = Now()
+	goroutine.goroutineCreateTime = time.Now()
+	goroutine.fromManager = g
+	g.mutex.Lock()
+	g.goroutineMap[goroutine.goroutineId] = goroutine
+	g.mutex.Unlock()
+	return goroutine
+}
+
+func (g *GoroutineManager) constructGoroutineWithCancel(goroutineName string) *Goroutine {
+	goroutine := new(Goroutine)
+	g.mutex.Lock()
+	g.counter++
+	goroutine.goroutineId = g.counter
+	g.mutex.Unlock()
+	goroutine.goroutineName = goroutineName
+	goroutine.goroutineCreateTime = time.Now()
+	ctx, cbCancel := context.WithCancel(context.Background())
+	goroutine.goroutineCtx = &ctx
+	goroutine.goroutineCancelFunc = &cbCancel
 	goroutine.fromManager = g
 	g.mutex.Lock()
 	g.goroutineMap[goroutine.goroutineId] = goroutine
@@ -62,11 +91,26 @@ func (g *GoroutineManager) GoroutineCreatePn(goroutineName string,
 	return goroutine.goroutineId
 }
 
+func (g *GoroutineManager) GoroutineCreateWithCancelPn(goroutineName string,
+	goroutineFunc func(Goroutine, ...interface{}),
+	argFunc ...interface{}) uint64 {
+	goroutine := g.constructGoroutineWithCancel(goroutineName)
+	go goroutineFunc(*goroutine, argFunc...)
+	return goroutine.goroutineId
+}
+
 func (g *GoroutineManager) GoroutineCreateP0(goroutineName string,
 	goroutineFunc func(Goroutine)) uint64 {
 	goroutine := g.constructGoroutine(goroutineName)
 	go goroutineFunc(*goroutine)
 	return goroutine.goroutineId
+}
+
+func (g *GoroutineManager) GoroutineCreateWithCancelP0(goroutineName string,
+	goroutineFunc func(Goroutine)) (uint64, context.CancelFunc) {
+	goroutine := g.constructGoroutineWithCancel(goroutineName)
+	go goroutineFunc(*goroutine)
+	return goroutine.goroutineId, *goroutine.goroutineCancelFunc
 }
 
 func (g *GoroutineManager) GoroutineCreateP1(goroutineName string,
@@ -77,12 +121,28 @@ func (g *GoroutineManager) GoroutineCreateP1(goroutineName string,
 	return goroutine.goroutineId
 }
 
+func (g *GoroutineManager) GoroutineCreateWithCancelP1(goroutineName string,
+	goroutineFunc func(Goroutine, interface{}),
+	argFunc1 interface{}) (uint64, context.CancelFunc) {
+	goroutine := g.constructGoroutineWithCancel(goroutineName)
+	go goroutineFunc(*goroutine, argFunc1)
+	return goroutine.goroutineId, *goroutine.goroutineCancelFunc
+}
+
 func (g *GoroutineManager) GoroutineCreateP2(goroutineName string,
 	goroutineFunc func(Goroutine, interface{}, interface{}),
 	argFunc1 interface{}, argFunc2 interface{}) uint64 {
 	goroutine := g.constructGoroutine(goroutineName)
 	go goroutineFunc(*goroutine, argFunc1, argFunc2)
 	return goroutine.goroutineId
+}
+
+func (g *GoroutineManager) GoroutineCreateWithCancelP2(goroutineName string,
+	goroutineFunc func(Goroutine, interface{}, interface{}),
+	argFunc1 interface{}, argFunc2 interface{}) (uint64, context.CancelFunc) {
+	goroutine := g.constructGoroutineWithCancel(goroutineName)
+	go goroutineFunc(*goroutine, argFunc1, argFunc2)
+	return goroutine.goroutineId, *goroutine.goroutineCancelFunc
 }
 
 func (g *GoroutineManager) GoroutineCreateP3(goroutineName string,
@@ -93,12 +153,28 @@ func (g *GoroutineManager) GoroutineCreateP3(goroutineName string,
 	return goroutine.goroutineId
 }
 
+func (g *GoroutineManager) GoroutineCreateWithCancelP3(goroutineName string,
+	goroutineFunc func(Goroutine, interface{}, interface{}, interface{}),
+	argFunc1 interface{}, argFunc2 interface{}, argFunc3 interface{}) (uint64, context.CancelFunc) {
+	goroutine := g.constructGoroutineWithCancel(goroutineName)
+	go goroutineFunc(*goroutine, argFunc1, argFunc2, argFunc3)
+	return goroutine.goroutineId, *goroutine.goroutineCancelFunc
+}
+
 func (g *GoroutineManager) GoroutineCreateP4(goroutineName string,
 	goroutineFunc func(Goroutine, interface{}, interface{}, interface{}, interface{}),
 	argFunc1 interface{}, argFunc2 interface{}, argFunc3 interface{}, argFunc4 interface{}) uint64 {
 	goroutine := g.constructGoroutine(goroutineName)
 	go goroutineFunc(*goroutine, argFunc1, argFunc2, argFunc3, argFunc4)
 	return goroutine.goroutineId
+}
+
+func (g *GoroutineManager) GoroutineCreateWithCancelP4(goroutineName string,
+	goroutineFunc func(Goroutine, interface{}, interface{}, interface{}, interface{}),
+	argFunc1 interface{}, argFunc2 interface{}, argFunc3 interface{}, argFunc4 interface{}) (uint64, context.CancelFunc) {
+	goroutine := g.constructGoroutineWithCancel(goroutineName)
+	go goroutineFunc(*goroutine, argFunc1, argFunc2, argFunc3, argFunc4)
+	return goroutine.goroutineId, *goroutine.goroutineCancelFunc
 }
 
 func (g *GoroutineManager) GoroutineRemove(goroutine *Goroutine) {
--- src/sched/goroutine_mgr/goroutine_mgr_test.go
+++ src/sched/goroutine_mgr/goroutine_mgr_test.go
@@ -10,37 +10,37 @@ import (
 func printn(g Goroutine, a ...interface{}) {
 	defer g.OnQuit()
 	fmt.Println("print", fmt.Sprintf("%d %d %d %d %d", a...))
-	time.Sleep(100000)
+	time.Sleep(100 * time.Microsecond)
 }
 
 func print0(g Goroutine) {
 	defer g.OnQuit()
 	fmt.Println("print0")
-	time.Sleep(100000)
+	time.Sleep(100 * time.Microsecond)
 }
 
 func print1(g Goroutine, a interface{}) {
 	defer g.OnQuit()
 	fmt.Println("print1", a)
-	time.Sleep(100000)
+	time.Sleep(100 * time.Microsecond)
 }
 
 func print2(g Goroutine, a interface{}, b interface{}) {
 	defer g.OnQuit()
 	fmt.Println("print2", a, b)
-	time.Sleep(100000)
+	time.Sleep(100 * time.Microsecond)
 }
 
 func print3(g Goroutine, a interface{}, b interface{}, c interface{}) {
 	defer g.OnQuit()
 	fmt.Println("print3", a, b, c)
-	time.Sleep(100000)
+	time.Sleep(100 * time.Microsecond)
 }
 
 func print4(g Goroutine, a interface{}, b interface{}, c interface{}, d interface{}) {
 	defer g.OnQuit()
 	fmt.Println("print4", a, b, c, d)
-	time.Sleep(100000)
+	time.Sleep(100 * time.Microsecond)
 }
 
 func TestAll(t *testing.T) {
@@ -71,4 +71,27 @@ func TestAll(t *testing.T) {
 	manager.GoroutineDump()
 	time.Sleep(10000000)
 	fmt.Println("goroutineCount", manager.GoroutineCount())
+}
+
+func select0(g Goroutine) {
+	defer g.OnQuit()
+	fmt.Println("select0")
+
+	select {
+	case <-(*g.GetContext()).Done():
+		fmt.Println("done/cancelled")
+	case <-time.After(3 * time.Second):
+		fmt.Println("timeout")
+	}
+}
+
+func TestCancel(t *testing.T) {
+	manager := new(GoroutineManager)
+	manager.Initialise("mgr2")
+
+	gid0, cbCancel := manager.GoroutineCreateWithCancelP0("goroutine"+strconv.Itoa(0), select0)
+	fmt.Println("gid0:", gid0)
+
+	time.Sleep(1 * time.Second)
+	cbCancel()
 }
